020544479d3b040f4701f7cadb43e29e7db27c5d
Anowar <1hafizajr@gmail.com>
2022-03-15 12:44:56 +0600

    add component

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="div">
+      <h2>Covid Tracker 22</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Header',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   content: ['./public/**/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {},
+    container: {
+      center: 'ture',
+      padding: '2rem',
+    }
   },
   plugins: [],
 }
